c49b8e369a029b4f614c0188e559d8c857248145
AnatolyaYtochka <130214900+AnatolyaYtochka@users.noreply.github.com>
2023-10-05 22:08:11 +0500

    Update task8.py

--- buns/mod2/task8.py
+++ buns/mod2/task8.py
@@ -1,5 +1,17 @@
-string = input()
-symbol = input()
+def getFragment(originalString):
+    string = ''
+    startIndex = 0
+    while originalString[startIndex] != ',':
+        string += originalString[startIndex]
+        startIndex += 1
+    return string
+
+originalString = input() + ','
+originalString = originalString.replace(' ', '')
+string = getFragment(originalString)
+originalString = originalString[len(string) + 1::]
+symbol = getFragment(originalString)
+
 count = 0
 while string[count] == symbol:
     count += 1
