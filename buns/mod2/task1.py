c4ca958b556fdbb70049719ba908f0efd1740038
AnatolyaYtochka <130214900+AnatolyaYtochka@users.noreply.github.com>
2023-10-05 22:06:49 +0500

    Update task1.py

--- buns/mod2/task1.py
+++ buns/mod2/task1.py
@@ -1,4 +1,15 @@
-a = int(input())
-b = int(input())
+def getNumbers(numbers):
+    number = ''
+    startIndex = 0
+    while numbers[startIndex] != ',':
+        number +=  numbers[startIndex]
+        startIndex += 1
+    return int(number)
+
+numbers = input() + ','
+numbers = numbers.replace(' ', '')
+a = getNumbers(numbers)
+numbers = numbers[len(str(a)) + 1::]
+b = getNumbers(numbers)
 c = a % b
 print(c)
