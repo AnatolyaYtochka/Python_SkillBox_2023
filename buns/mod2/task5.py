c87b90145bb9cc95dd2d230fb157268eb2df5ec6
AnatolyaYtochka <130214900+AnatolyaYtochka@users.noreply.github.com>
2023-10-05 22:07:36 +0500

    Update task5.py

--- buns/mod2/task5.py
+++ buns/mod2/task5.py
@@ -1,5 +1,17 @@
-symbol = ord(input())
-shift = int(input())
+def getFragment(originalString):
+    string = ''
+    startIndex = 0
+    while originalString[startIndex] != ',':
+        string += originalString[startIndex]
+        startIndex += 1
+    return string
+
+originalString = input() + ','
+originalString = originalString.replace(' ', '')
+symbol = ord(getFragment(originalString))
+originalString = originalString[2::]
+shift = int(getFragment(originalString))
+
 newSymbol = symbol + shift
 if newSymbol > 122:
     newSymbol = newSymbol % 122 + 96
