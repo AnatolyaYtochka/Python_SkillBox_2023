c323fab80087a4dd61a47070f6a4d6d6fb9e975d
AnatolyaYtochka <130214900+AnatolyaYtochka@users.noreply.github.com>
2023-11-12 23:31:05 +0500

    Update task3.py

--- buns/mod7/task3.py
+++ buns/mod7/task3.py
@@ -4,22 +4,27 @@ import time
 def timer(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        started_ad = time.time()
+        start = time.time()
         result = func(*args, **kwargs)
-        ended_ed = time.time()
-        run_time = ended_ed - started_ad
+        end = time.time()
+        run_time = end - start
         print(f"Функция {func.__name__} работала {run_time} секунд")
         return result
     return wrapper
 
-@timer
+
 def fibonacci(n):
     if n < 2:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(100))
-"""
+@timer
+def run_fibonacci(n):
+    print(fibonacci(n))
+
+run_fibonacci(20)
+
+
 def memoize(func):
     cache = {}
     def wrapper(*args):
@@ -28,12 +33,14 @@ def memoize(func):
         return cache[args]
     return wrapper
 
-@timer
 @memoize
 def fibonacci(n):
     if n < 2:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(100))"""
+@timer
+def run_fibonacci_with_memoize(n):
+    print(fibonacci(n))
 
+run_fibonacci_with_memoize(20)
